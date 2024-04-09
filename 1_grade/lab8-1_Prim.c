bf5ec8665e2c025502ff54d694258c5ec8d8c54b
Likkery <72561892+likkery@users.noreply.github.com>
2024-04-09 20:51:19 +0700

    prim done

--- 1_grade/lab8-1_Prim.c
+++ 1_grade/lab8-1_Prim.c
@@ -4,74 +4,30 @@
 
 #define MAX_CNT 5000
 #define GRAPH struct graph
+#define MIN(i, j) (((i) < (j)) ? (i) : (j))
 
 
 GRAPH{
-    int start;
-    int end;
-    long long int weight;
+    int nodeCnt;
+    int edgeCnt;
+    long long int *weight;
 };
 
 
 // check the data while add it to the structure
-int checkEdge(GRAPH edge, int nodeCnt) {
-    if (edge.start < 1 || edge.start > nodeCnt ||
-    edge.end < 1 || edge.end > nodeCnt){
+int checkEdge(int start, int end, int weight, int nodeCnt) {
+    if (start < 1 || start > nodeCnt ||
+    end < 1 || end > nodeCnt){
         printf("bad vertex");
         return 1;
     }
 
-    if (edge.weight < 0 || edge.weight > INT_MAX){
+    if (weight < 0 || weight > INT_MAX){
         printf("bad length");
         return 1;
     }
 
     return 0;
-}
-
-
-// comparator for qsort
-int edgeCompare(const void* first, const void* second){
-    return ((GRAPH*)first) -> weight - ((GRAPH*)second) -> weight;
-}
-
-
-//find parent of node
-int findParent(int node, int parent[]){
-    if (parent[node] == -1)
-        return node;
-    return findParent(parent[node], parent);
-}
-
-
-// create graph structure
-GRAPH *create(GRAPH *edge, int edgeCnt, int nodeCnt) {
-    edge = (GRAPH*)malloc(edgeCnt * sizeof(GRAPH));
-    int flag = 0, lines = 0;
-
-    for (lines; lines < edgeCnt; lines++) {
-        if (scanf("%d %d %lld", &edge[lines].start, &edge[lines].end,
-        &edge[lines].weight) != 3) {
-            printf("bad number of lines");
-            exit(0);
-        }
-
-        if (checkEdge(edge[lines], nodeCnt))
-            exit(0);
-
-        if (edge[lines].end == nodeCnt || edge[lines].start == nodeCnt)
-            flag++;
-
-        edge[lines].start--;
-        edge[lines].end--;
-    }
-
-    if (flag == 0 && nodeCnt != 1){
-        printf("no spanning tree");
-        exit(0);
-    }
-
-    return edge;
 }
 
 
@@ -89,76 +45,119 @@ int check(int nodeCnt, int edgeCnt){
 
     return 0;
 }
-// swap nodes
-void swap (GRAPH *a, GRAPH *b) {
-    GRAPH c = *a;
-    *a = *b;
-    *b = c;
-}
-// partition for quicksort
-int partition (GRAPH* arr, int low, int hight) {
-    int mid = arr[(low + hight) / 2].weight;
-    int i = low, j = hight;
-    while (1) {
-        while (arr[i].weight < mid)
-            i++;
-        while (arr[j].weight > mid)
-            j--;
-        if (i >= j) {
-            return j;
+
+
+// create graph structure
+GRAPH *create(GRAPH *graph, int edgeCnt, int nodeCnt) {
+    graph = (GRAPH*)malloc(sizeof(GRAPH));
+    int flag = 0, lines = 0, start, end;
+    long long int weight;
+
+    graph->edgeCnt = edgeCnt;
+    graph->nodeCnt = nodeCnt;
+    graph->weight = (long long int*)calloc(nodeCnt * nodeCnt, 
+    sizeof(long long int));
+
+    for (lines; lines < edgeCnt; lines++) {
+        if (scanf("%d %d %lld", &start, &end, &weight) != 3) {
+            printf("bad number of lines");
+            exit(0);
         }
-        swap(&arr[i++], &arr[j--]);
-    }
-}
 
-void quicksort (GRAPH* arr, int low, int hight) {
-    if (low < hight) {
-        int part = partition(arr, low, hight);
-        quicksort(arr, low, part);
-        quicksort(arr, part + 1, hight);
-    }
-}
+        if (checkEdge(start, end, weight, nodeCnt))
+            exit(0);
 
+        if (end == nodeCnt || start == nodeCnt)
+            flag++;
 
-// kruskal algoritm
-void kruskal(GRAPH* edge, int edgeCnt, int nodeCnt){
-    int edges = 0, start, end,
-    *parents = (int*)malloc(nodeCnt * sizeof(int));
-    GRAPH* frame = (GRAPH*)malloc(nodeCnt * sizeof(GRAPH));
-    quicksort(edge, 0, edgeCnt - 1);
+        start--;
+        end--;
 
-    for (int i = 0; i < nodeCnt; i++)
-        parents[i] = -1;
-
-    for (int i = 0; i < edgeCnt && edges < nodeCnt;){
-        GRAPH current = edge[i++];
-        start = findParent(current.start, parents);
-        end = findParent(current.end, parents);
-
-        if (start != end){
-            frame[edges++] = current;
-            parents[start] = end;
-        }
+        graph->weight[nodeCnt * start + end] = weight;
+        graph->weight[nodeCnt * end + start] = weight;
     }
 
-    if (edges < nodeCnt - 1)
+    if (flag == 0 && nodeCnt != 1){
         printf("no spanning tree");
-    else
-        for (int i = 0; i < nodeCnt - 1; i++) {
-            if (frame[i].start < frame[i].end)
-                printf("%d %d\n", frame[i].start + 1, frame[i].end + 1);
-            else
-                printf("%d %d\n", frame[i].end + 1, frame[i].start + 1);
+        exit(0);
+    }
+
+    return graph;
+}
+
+
+//free all structures
+void freeMemory(int *visited, int *frame, int *minEdges, GRAPH *graph){
+    free(visited);
+    free(frame);
+    free(minEdges);
+    free(graph->weight);
+    free(graph);
+}
+
+
+// find next min edge
+int minEdge(GRAPH *graph, int *visited, int *minEdge){
+    int min = INT_MAX, minInd;
+
+    for (int i = 0; i < graph->nodeCnt; i++)
+        if (!visited[i] && minEdge[i] < min) {
+            min = minEdge[i];
+            minInd = i;
         }
 
-    free(parents);
-    free(frame);
+    return minInd;
+}
+
+
+// prim algoritm
+void prim(GRAPH* graph, int nodeCnt){
+    int *visited = (int*)malloc(nodeCnt * sizeof(int)),
+    *minEdges = (int*)malloc(nodeCnt * sizeof(int)),
+    *frame = (int*)malloc(nodeCnt * sizeof(int));
+
+    for (int i = 0; i < nodeCnt; i++){
+        visited[i] = 0;
+        minEdges[i] = INT_MAX;
+        frame[i] = -1;
+    }
+
+    minEdges[0] = 0;
+
+    for (int i = 0; i < nodeCnt; i++) {
+        int min = minEdge(graph, visited, minEdges);
+        visited[min] = 1;
+
+        for (int j = 0; j < nodeCnt; j++){
+            if (!visited[j] && graph->weight[nodeCnt * min + j] 
+            && graph->weight[nodeCnt * min + j] < minEdges[j]){
+                frame[j] = min + 1;
+                minEdges[j] = graph->weight[nodeCnt * min + j];
+            }
+        }
+    }
+
+    for (int i = nodeCnt; i > 0; i--)
+        if (minEdges[i] == INT_MAX){
+            printf("no spanning tree");
+            freeMemory(visited, frame, minEdges, graph);
+            exit(0);
+        }
+
+    for (int i = 1; i < nodeCnt; i++){
+        if (frame[i] + 1 < i + 1)
+            printf("%d %d\n", frame[i], i + 1);
+        else
+            printf("%d %d\n", i + 1, frame[i]);
+    }
+
+    freeMemory(visited, frame, minEdges, graph);
 }
 
 
 int main() {
     int nodeCnt, edgeCnt, edgeLen;
-    GRAPH *edge = NULL;
+    GRAPH *graph = NULL;
 
     scanf("%d", &nodeCnt);
     scanf("%d", &edgeCnt);
@@ -166,9 +165,9 @@ int main() {
     if (check(nodeCnt, edgeCnt))
         return 0;
 
-    edge = create(edge, edgeCnt, nodeCnt);
-    kruskal(edge, edgeCnt, nodeCnt);
+    graph = create(graph, edgeCnt, nodeCnt);
+    prim(graph, nodeCnt);
 
-    free(edge);
+    free(graph);
     return 0;
 }
